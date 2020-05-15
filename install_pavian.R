39bc418b778d03e479b43c4e2fe93f80c7445617
cwright <chris.wright@nanoporetech.com>
2020-05-15 15:06:07 +0100

    Add pavian and update bits and pieces

---
+++ install_pavian.R
@@ -0,0 +1,11 @@
+#!/usr/bin/Rscript
+
+### this installs but isn't picked up by pavian
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("GenomicRanges")
+#install.packages("BiocManager")
+#BiocManager::install("Rsamtools")
+
+install.packages("remotes")
+remotes::install_github("fbreitwieser/pavian", upgrade=T, quiet=T)
+
