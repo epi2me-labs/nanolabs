8bce820c277b2ed57d242944189b77aab49641b6
Chris Wright <chris.wright@nanoporetech.com>
2020-10-23 17:45:43 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,15 @@
 #   This is run by start.sh from docker-stats
 #   The docker file installs it to /usr/local/bin/start-notebook.d/
 
+
 MOUNT=/epi2melabs/
+NOTEBOOKDIR=${MOUNT}/notebooks/
 
+# new notebooks from templates are saved here:
+mkdir -p ${NOTEBOOKDIR}
 
-mkdir -p ${MOUNT}/notebooks/
+# trust any notebooks in the directory where we save
+for file in $(find ${NOTEBOOKDIR} -name "*.ipynb"); do
+    echo "Adding trust: ${file}"
+    jupyter trust ${file}
+done 
